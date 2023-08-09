70e957cf653bbb5b4bdcfea77aff47550dc60b14
Casca Kwok <casca.kwok@gmail.com>
2023-08-08 21:17:29 -0700

    a5 REST su23

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ import "dotenv/config";
 const app = express()
 
 const allowedOrigins = [
-  'https://a5--sensational-nougat-1bbd5b.netlify.app',
-  'http://localhost:3000',
+  /*'https://a5--sensational-nougat-1bbd5b.netlify.app',
+  'http://localhost:3000',*/
+  process.env.FRONTEND_URL,
 ];
 
-console.log("process.env.FRONTEND_URL", process.env.FRONTEND_URL);
+console.log("process.env.FRONTEND_URL2", process.env.FRONTEND_URL);
 app.use(
   cors({
     credentials: true,
